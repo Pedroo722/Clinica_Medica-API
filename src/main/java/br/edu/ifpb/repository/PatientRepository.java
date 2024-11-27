f777135fcb3cf82b8d7f24270713cad581e161e4
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2024-11-27 18:30:36 -0300

    feature: Metodo findByPatientId de RecordRepository

--- src/main/java/br/edu/ifpb/repository/PatientRepository.java
+++ src/main/java/br/edu/ifpb/repository/PatientRepository.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import br.edu.ifpb.model.Patient;
 
 @Repository
--- src/main/java/br/edu/ifpb/repository/RecordRepository.java
+++ src/main/java/br/edu/ifpb/repository/RecordRepository.java
@@ -1,9 +1,13 @@
 package br.edu.ifpb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import br.edu.ifpb.model.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    List<Record> findByPatientId(Long pacienteId);
 }
