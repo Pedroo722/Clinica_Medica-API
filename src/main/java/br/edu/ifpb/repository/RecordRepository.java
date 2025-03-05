c02d808aed7820fc0484d54561a7181b77d4760e
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2025-03-05 12:51:54 -0300

    fix: Correção no metodo findByPacienteId

--- src/main/java/br/edu/ifpb/repository/RecordRepository.java
+++ src/main/java/br/edu/ifpb/repository/RecordRepository.java
@@ -1,6 +1,6 @@
 package br.edu.ifpb.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +9,5 @@ import br.edu.ifpb.model.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
-    public Optional<Record> findByPacienteId(Long pacienteId);
+    public List<Record> findByPacienteId(Long pacienteId);
 }
